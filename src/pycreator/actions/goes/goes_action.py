fb28c7b236290f0aac4a5c7ecd5828c147545093
Nex Sabre <NexSabre@users.noreply.github.com>
2020-09-23 22:21:42 +0200

    Update a goes cmd

--- src/pycreator/actions/goes/goes_action.py
+++ src/pycreator/actions/goes/goes_action.py
@@ -3,12 +3,7 @@ from pycreator.actions.create.create_action import CreateAction
 
 class GoesAction(CreateAction):
     ACTION = "goes"
-    # PARAM_NAME = "ACTION"
-    #
-    # def fill_parser_arguments(self):
-    #     self.parser.add_argument('-n', '--name', type=str, help='Name for the new project', required=True)
-    #     self.parser.add_argument('-l', '--location', type=str, help='Custom location to create a new app', default=None)
-    #
-    # def process_action(self, configuration):
-    #     target_location = configuration.location if configuration.location else os.getcwd()
-    #     CreateAction.create_boilerplate(target_location, configuration.name)
+
+    def fill_parser_arguments(self):
+        self.parser.add_argument('name', type=str, help='Name for the new project')
+        self.parser.add_argument('-l', '-at', '--location', type=str, help='Custom location to create a new app')
