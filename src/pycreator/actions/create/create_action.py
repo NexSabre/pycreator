646a8af1349c828c07b20f48f8435ab75342d136
Nex Sabre <NexSabre@users.noreply.github.com>
2020-10-15 21:20:07 +0200

    Initial commit for support of simple flask app

--- src/pycreator/actions/create/create_action.py
+++ src/pycreator/actions/create/create_action.py
@@ -3,6 +3,12 @@ import os
 from pycreator.actions.action import Action
 from pycreator.core import create_folders
 from pycreator.framework.messages import Messages
+from enum import Enum
+
+
+class TypeChoices(Enum):
+    CMD = "cmd"
+    FLASK = "flask"
 
 
 class CreateAction(Action):
@@ -12,17 +18,32 @@ class CreateAction(Action):
     def fill_parser_arguments(self):
         self.parser.add_argument('-n', '--name', type=str, help='Name for the new project', required=True)
         self.parser.add_argument('-l', '--location', type=str, help='Custom location to create a new app', default=None)
+        self.parser.add_argument('-t', '--type',
+                                 choices=list(map(lambda t: t.value, TypeChoices)),
+                                 default=TypeChoices.CMD)
 
     def process_action(self, configuration):
         target_location = configuration.location if configuration.location else os.getcwd()
-        if self.check_app_exists(target_location, configuration.name):
+
+        create_app = self.choose_application_type(TypeChoices(configuration.type))
+        create_app(target_location, configuration.name)
+
+    def choose_application_type(self, app_type):
+        return {TypeChoices.CMD: self.create_command_app,
+                TypeChoices.FLASK: self.create_flask_app}.get(app_type)
+
+    def create_command_app(self, target_location: str, app_name: str) -> None:
+        if self.check_app_exists(target_location, app_name):
             exit(1)
-        if not self.create_boilerplate(target_location, configuration.name):
-            Messages.error(f"Some error occur. The pycreator does not create an application {configuration.name}"
+        if not self.create_boilerplate(target_location, app_name):
+            Messages.error(f"Some error occur. The pycreator does not create an application {app_name}"
                            f" at {target_location}")
             return
-        Messages.ok(f"The pycreator successfully create a new application {configuration.name}"
+        Messages.ok(f"The pycreator successfully create a new application {app_name}"
                     f" at {target_location}")
+
+    def create_flask_app(self, target_location: str, app_name: str) -> None:
+        raise NotImplemented
 
     @staticmethod
     def create_boilerplate(tgt_location: str, app_name: str):
